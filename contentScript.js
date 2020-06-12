1d0abdac436ced8611c122c5a34309bf866a59e8
rjpeterson <r.james.peterson@gmail.com>
2020-06-12 22:24:42 +0800

    It works! Next up: scaling line height

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,15 @@
-var hanzisizeUtil = {
-  test_mode: true,
+let test_mode = true;
+
+const hanzisizeUtil = {
   REGEX_CHINESE: /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u,
   REGEX_ENGLISH: /^[a-zA-Z0-9$@$!%*?&#^-_. +]+$/,
 
-  // Error & function handling for language selection
-  hasLanguage: function(lang, str) {
+  // Error & function handling for language selection 
+  // 6/12/2020 LANGUAGE SELECTION NOT YET IMPLEMENTED INTO POPUP
+  hasLanguage(lang, str) {
     switch (lang) {
       case 'English':
         return hanzisizeUtil.REGEX_ENGLISH.test(str);
-      // case 'Portuguese':
-      //   throw new Error('Sorry, Portuguese function not yet added')
       case 'Chinese':
         return hanzisizeUtil.REGEX_CHINESE.test(str);
       default:
@@ -18,142 +18,113 @@ var hanzisizeUtil = {
   },
 
   // returns all elements with text of chosen language with optional callback for each step of loop
-  getLangText: function(document, language, callback, newMinFontSize) {
-    if(hanzisizeUtil.test_mode) {console.log(`newMinFontSize: ${newMinFontSize}   initiating getLangText...`)}
+  getLangText(document, language, singleElementResizerCallback, newMinFontSize) {
+    let languagePass;
     const langElems = [];
+
+    if(test_mode) {
+      console.log(`newMinFontSize: ${newMinFontSize} | initiating getLangText...`)
+    }
+
     // get all elements in DOM
     const allElems = document.getElementsByTagName('*')
-    // loop through all elements
     for (let i = 0; i < allElems.length; i++) {
-      const el = allElems[i]
-      // if element has content that is not nested
-      if (el.firstChild) {
-        if(hanzisizeUtil.test_mode) {console.log(`content of element's first child : ${el.firstChild.nodeValue}`)}
-        // if language of content matches input language
-        let languagePass;
-        try {
-          languagePass = hanzisizeUtil.hasLanguage(language, el.firstChild.nodeValue)
+      const el = allElems[i];
+      if (el.firstChild) {// if element has content that is not nested
+        const elText = el.firstChild.nodeValue;
+
+        if(test_mode) {console.log(`content of element's first child : ${elText}`)};
+        
+        try { // check if content matches input language
+          languagePass = hanzisizeUtil.hasLanguage(language, elText)
         } catch (error) {
-          console.log('hasLanguage function error:' + error)
-        }
+          if(test_mode) console.log('hasLanguage function error:' + error)
+        };
+
         if (languagePass) {
-          if(hanzisizeUtil.test_mode) {console.log(`Element contains ${language} text`)}
-          langElems.push(el.firstChild.nodeValue);
-          // pass in resizeSingleElement function as callback
-          if (callback) {
-            if(hanzisizeUtil.test_mode) {console.log(`passing element and NMFS to callback function. NMFS: ${newMinFontSize}`)}
-            callback(window, language, el, newMinFontSize)
+          if(test_mode) {console.log(`Element contains ${language} text`)};
+          langElems.push(elText);
+
+          // singleElementResizer function to be passed in as callback
+          if (singleElementResizerCallback) {
+            if(test_mode) {console.log(`passing element and NMFS to callback function. NMFS: ${newMinFontSize}`)}
+            singleElementResizerCallback(window, el, newMinFontSize)
           };
-        } else {
-          // console log for debugging
-          if(hanzisizeUtil.test_mode) {console.log(`Case 1: element does not contain ${language} text`)}
+        } else { // languagePass == false
+          if(test_mode) {console.log(`case 1: element does not contain ${language} text`)}
         }
       }
-    }
-    return langElems;
+    } //end for loop
+    return langElems; //mainly for testing purposes
   },
 
-  // input a single element, target lang, and MFS, function will perform proper resizing procedure
-  singleElementResizer: function(window, language, el, newMinFontSize) {
-    // const newMinFontSize = parseInt(newMinFontSizeRaw);
-    // const languageMatch = hanzisizeUtil.hasLanguage(language, el.firstChild.nodeValue);
+  // input window, single element and NewMinFS, function will assess OFS, NMFS, CFS & perform proper resizing procedure
+  //case 1: element does not contain ${language} text (getLangText function)
+  //case 2: CurrentFS is original. No change
+  //case 3: CurrentFS is not original and larger than NewMinFS. FS set to OriginalFS
+  //case 4: CurrentFS is not original and less than or equal to NewMinFS. FS to NewMinFS
+  //case 5: No OriginalFS. Save CurrentFS as OriginalFS. Set FS to NewMinFS
+  //case 6: OriginalFS && (CurrentFS < NMFS), Set FS to NewMinFS
+  singleElementResizer(window, el, newMinFontSize) {
     const currentFontSize = parseInt(window.getComputedStyle(el, null).getPropertyValue('font-size'));
-    const originalFontSize = parseInt(window.getComputedStyle(el, null).getPropertyValue('data-original-font-size'));
+    const originalFontSize = parseInt(window.getComputedStyle(el, null).getPropertyValue('--data-original-font-size'));
 
-      if(hanzisizeUtil.test_mode) {
+      if(test_mode) {
         console.log(`initiating singleElementResizer`)
-        // console.log(`language match :${languageMatch}`);
         console.log(`current font size: ${currentFontSize}`)
         console.log(`new min font size: ${newMinFontSize}`)
         if(isNaN(originalFontSize)) {
           console.log(`originalFontSize not found`);
         } else {
           console.log(`originalFontSize: ${originalFontSize}`);
-        };
-        console.log('assessing case...')
+        }
       }
 
-    // if (!languageMatch) {
-    //   // case 1: !language match -> no change
-    //   if(hanzisizeUtil.test_mode) {console.log(`case 1: No text matching language "${language}" found`)};
-    //   return
-    // } else { // language match
     if (currentFontSize == newMinFontSize) {
-      console.log('CFS equals NMFS. no change');
-    } else if (currentFontSize > newMinFontSize) { // from big text to small text
-      if(hanzisizeUtil.test_mode) {console.log(`CFS is larger than NMFS`)}
+      if(test_mode) console.log('CFS equals NMFS. no change');
+
+    } else if (currentFontSize > newMinFontSize) { // from big to small
+      if(test_mode) {console.log(`CurentFS is larger than NewMinFS`)}
+
       if (!originalFontSize) { 
-        // case 2: !OFS && NMFS < CFS, likely header text -> no change
-        if(hanzisizeUtil.test_mode) {console.log(`case 2: CFS is original. No change`)}
+        if(test_mode) {console.log(`case 2: CurrentFS is original. No change`)}
         return
       } else if (originalFontSize > newMinFontSize) { 
-        // case 3: NMFS < (OFS&CFS) text should never be smaller than OFS -> change
+        if(test_mode) {console.log(`case 3: CurrentFS is not original and larger than NewMinFS. FS set to OriginalFS`)};
         el.style.setProperty('font-size', originalFontSize + 'px', 'important');
-        if(hanzisizeUtil.test_mode) {console.log(`case 3: CFS is not original and larger than NMFS. FS to OFS`)};
-      } else if (originalFontSize < newMinFontSize) {
-        // case 4: OFS < NMFS < CFS text size reduced to NMFS -> change
-        el.style.setProperty('font-size', newMinFontSize + 'px', 'important');
-        if(hanzisizeUtil.test_mode) {console.log(`case 4: CFS is not original and less than NMFS. FS to NMFS`)};
-      }
-    } else { // CFS < NMFS   small -> big
-      if(hanzisizeUtil.test_mode) {console.log(`CFS is less than NMFS`)}
-      if (!originalFontSize) {
-        // case 5: !OFS && CFS < NMFS, retain original font size for later use -> change
-        el.style.setProperty('data-original-font-size', currentFontSize + 'px');
-        el.style.setProperty('font-size', newMinFontSize + 'px', 'important');
-        if(hanzisizeUtil.test_mode) {console.log(`case 5: CFS is original. Save CFS as OFS. Set FS to NMFS`)};
       } else {
-      // case 6: OFS && CFS < NMFS, enlarge text -> change
+        if(test_mode) {console.log(`case 4: CurrentFS is not original and less than or equal to NewMinFS. FS to NewMinFS`)};
+        el.style.setProperty('font-size', newMinFontSize + 'px', 'important');
+      }
+    } else { // CFS < NMFS   from small to big
+      if(test_mode) {console.log(`CFS is less than NMFS`)}
+      if (!originalFontSize) {
+        if(test_mode) {console.log(`case 5: No OriginalFS. Save CurrentFS as OriginalFS. Set FS to NewMinFS`)};
+        el.style.setProperty('--data-original-font-size', currentFontSize + 'px');
+        el.style.setProperty('font-size', newMinFontSize + 'px', 'important');
+      } else {
+      if(test_mode) {console.log(`case 6: OriginalFS && (CurrentFS < NMFS), Set FS to NewMinFS`)};
       el.style.setProperty('font-size', newMinFontSize + 'px', 'important');
-      if(hanzisizeUtil.test_mode) {console.log(`case 6: Set FS to NMFS`)};
       }
     }
-    // }
   },
 
   // primary function for extension
-  main: function(language, minFontSize) {
-    console.log(`initiating main function...`);
+  main(language, minFontSize) {
+    if(test_mode) console.log(`initiating main function...`);
     try {
       hanzisizeUtil.getLangText(document, language, hanzisizeUtil.singleElementResizer, minFontSize)
     }
-    catch(err) {console.log(`main fuction failed: ${err}`)}
+    catch(err) {console.log(`Hanzisize failed: ${err}`)}
   }
 }
 
 chrome.runtime.onMessage.addListener(
 	function(obj, sender, sendResponse) {
-    	
-      if (hanzisizeUtil.test_mode) {
+      if (test_mode) {
         console.log('object received by contentScript:' + JSON.stringify(obj) + 'Resizing now...')
       }
-      hanzisizeUtil.main(obj.language, obj.minFontSize)
-		
+
+      hanzisizeUtil.main(obj.language, obj.newMinFontSize)
       sendResponse({farewell: "From content: I got the object."});
   });
-// console.log(`running content script with args "Chinese", 30`)
-// hanzisizeUtil.main(language, newMinFontSize);
-
-  // // inputs a single element and a minumum font size. Sets font-size attribute of  element if current size is less than minFontSize
-  // resizeSingleElement(el, newMinFontSize) {
-  //   // get font-size of element
-  //   const currentFontSize = parseFloat(document.defaultView.getComputedStyle(el, null).getPropertyValue('font-size'));
-  //   // compare font-size of element to newMinFontSize
-  //   if (currentFontSize >= newMinFontSize) { 
-  //     // if current text is originally larger than what we need, do nothing
-  //     return
-  //   } else {
-  //     hanzisizeUtil.manageFontSize(el, newMinFontSize);
-  //   }
-
-  //   if (newMinFontSize > oldFontSize) {
-  //     if (!el.isContentEditable || (el.parentNode && !el.parentNode.isContentEditable)) { // Version 1.2.7 - Fix contentEditable (gmail etc) - Changing to bold grows bigger
-  //       el.style.setProperty('font-size', newMinFontSize + 'px', 'important')
-  //       // Version 1.3.0 - Change line-height for websites like nytimes.com
-  //       const curFontSize = parseInt(document.defaultView.getComputedStyle(el, null).getPropertyValue('font-size'))
-  //       // const curLineHeight = parseInt(document.defaultView.getComputedStyle(el, null).getPropertyValue('line-height'))
-  //       // if (curLineHeight !== 'normal' && curLineHeight <= (curFontSize + 1)) {
-  //       //   el.style.setProperty('line-height', 'normal', 'important')
-  //     }
-  //   }
-  // },
--- popup.js
+++ popup.js
@@ -1,133 +1,112 @@
 let tab_id = false;
 let test_mode = true;
-let minFontSize;
+let min_font_size;
 
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-	var tab = tabs[0];
-  tab_id = tab.id;
-
-	if (test_mode) console.log(tab.url); // Version 1.3.0
-	if (tab.url.match(/^chrome/i))
-		document.getElementById('error').innerHTML = "NOTE: Google blocks extenstions and does not allow them to work on special <b>chrome://</b> pages such as the current page.";
-    else if (tab.url.match(/\/webstore/i))	
-		document.getElementById('error').innerHTML = "NOTE: For this extension to work you must leave the Chrome Webstore and go to another website. Google blocks extensions from functioning on special Google pages such as the Chrome Webstore.";
-});
-
-function send_to_content(obj)
-{
+// accepts language and newMinFontSize object and sends it to content script
+function send_to_content(obj) {
 	if (test_mode) console.log('tab_id:' + tab_id);
-	//console.log(JSON.stringify(obj));
-	// Version 1.2.5 - Added {frameId: 0} to sendMessage only to the main frame in the tab
+
+  // try to send object to content script
 	chrome.tabs.sendMessage(tab_id, obj, {frameId: 0}, function(response) {
-    	 
-    	if (chrome.runtime.lastError) 
-    	{
-            console.log('ERROR: ' + chrome.runtime.lastError.message);
-        /* Above gets "ERROR: Could not establish connection. Receiving end does not exist"
-			if you run it on chrome://extensions/ page or if the extension is just installed
-			and the page has not been refreshed */
-			//if (chrome.runtime.lastError.match(/Receiving end does not exist/i))
-			//	document.getElementById('error').innerHTML = "NOTE: You must refresh the web page before this extension will work on it!";
-		
-			/*  Inject content.js in active tab.
-					Need "permissions": ["activeTab"] in manifest.json for this to work
-			*/
-			 	chrome.tabs.executeScript(tab_id, {file: "contentScript.js"}, function() {
-    				if (chrome.runtime.lastError) {
-        				console.error(chrome.runtime.lastError.message);
-    				}
-    				else
-    				{
-    					setTimeout(function(){ 
-							send_to_content(obj); // Then call send_to_content(obj) again after content.js injected
-						}, 200);
-    				}
-    			});		
+    // if fails to send, inject script and send again
+    if (chrome.runtime.lastError) {
+      if (test_mode) console.log('ERROR: ' + chrome.runtime.lastError.message);
+      // Above gets "ERROR: Could not establish connection. Receiving end does not exist" if you run it on chrome://extensions/ page or if the extension is just installed and the page has not been refreshed
+    
+      // Inject content.js in active tab. Need "permissions": ["activeTab"] in manifest.json to work
+      chrome.tabs.executeScript(tab_id, {file: "contentScript.js"}, function() {
+        if (chrome.runtime.lastError) {
+          if (test_mode) console.error(chrome.runtime.lastError.message);
+        } else {
+          setTimeout(function(){ 
+            // call send_to_content(obj) again after content.js injected
+            send_to_content(obj); 
+          }, 200);
+        }
+      });		
 		}
-		console.log(JSON.stringify(response));
+		if (test_mode) console.log(JSON.stringify(response));
 
   	});
-  	//tab_id = false;
 }
 
 // function to push submitted fontsize to chrome local storage
 const pushToStorage = async (newMinFontSize) => {
   await chrome.storage.local.set({minFontSize: newMinFontSize}, () => {
-    console.log(`New minimum font size stored as: ${newMinFontSize}`)
+    if (test_mode) console.log(`New minimum font size stored as: ${newMinFontSize}`)
   })
 }
 
-// function to retrieve min font size from chrome local storage
-const getFromStorage = async (el, callback) => {
+// function to retrieve min font size from chrome local storage and display in popup if callback provided
+const getFromStorage = async (displayCallback, el) => {
   await chrome.storage.local.get('minFontSize', (result) => {
-    console.log(`Retrieved min font size from storage: ${result.minFontSize}`)
-    minFontSize = result.minFontSize
-    if (callback) {
-      callback(el, minFontSize)
+    if (test_mode) console.log(`Retrieved min font size from storage: ${result.minFontSize}`)
+    min_font_size = result.minFontSize
+    if (displayCallback) {
+      try { displayCallback(el, min_font_size) }
+      catch(err) {
+        console.log(`Failed to retrieve new font size from storage. ${err}`)
+      }
     }
   })
 };
 
-// used as callback for chrome storage get to display value in popup
+// used as callback for getFromStorage to display value in popup
 const display = (el, value) => {
-  // console.log(`result: ${value}`)
   el.html(value)
 };
 
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+  var tab = tabs[0];
+  // get current tab id
+  tab_id = tab.id;
+
+  if (test_mode) console.log(tab.url);
+  
+	if (tab.url.match(/^chrome/i))
+		document.getElementById('error').innerHTML = "NOTE: Google blocks extensions and does not allow them to work on special <b>chrome://</b> pages such as the current page.";
+    else if (tab.url.match(/\/webstore/i))	
+		document.getElementById('error').innerHTML = "NOTE: For this extension to work you must leave the Chrome Webstore and go to another website. Google blocks extensions from functioning on special Google pages such as the Chrome Webstore.";
+});
+
 $(document).ready(() => {
-  console.log("popup.js loaded...");
+  if (test_mode) console.log("popup.js loaded...");
   const newFontSizeInput = $('#min-font-size');
   const savedFontSize = $('#curr-saved-font-size');
   const errorMessage = $('#error-message');
 
-  // Initial filling of value if present
-  try {
-    getFromStorage(savedFontSize, display);
-  } catch(err) {
-    console.log(`Failed to retrieve new font size from storage. ${err}`)
-  };
+  // If there is a font size saved in storage, display it
+  getFromStorage(display, savedFontSize);
 
   $('#submit').on('click', async () => {
     // validate input as positive number
     const newFontSize = Number(newFontSizeInput.val());
-    minFontSize = newFontSize;
+    min_font_size = newFontSize;
     if (newFontSize <= 0) {
       errorMessage.html('Font Size must be a positive number')
       return;
     }
 
     // error logging & storing in chrome storage
-    console.log(`submitted font size: ${newFontSize}`);
+    if(test_mode) console.log(`submitted font size: ${newFontSize}`);
     try {pushToStorage(newFontSize)}
     catch(err) {console.log(`Failed to store new font size. ${err}`)};
+    // retrieve font size just stored and deisplay in popup
+    getFromStorage(display, savedFontSize);
 
     try {
-      getFromStorage(savedFontSize, display);
-    } catch(err) {
-      console.log(`Failed to retrieve new font size from storage. ${err}`)
-    };
-
-    try {
-      console.log(`should start main function`);
       let obj = {
         'language' : 'Chinese',
-        'newMinFontSize': minFontSize
+        'newMinFontSize': min_font_size
       };
-      console.log('sending obj to content script:' + JSON.stringify(obj));
+      if(test_mode) console.log('sending obj to content script:' + JSON.stringify(obj));
       send_to_content(obj);
-      // chrome.tabs.executeScript(null, { 
-      //   // set variables for font resizing function
-      //   code: `let language = 'Chinese'; let newMinFontSize = ${getFromStorage()}`
-      // }, function() {
-      //   // call font resizing function
-      //   chrome.tabs.executeScript(null, {file: 'contentScript.js'});
-      // });
     } catch(err) {
-      console.log(`Failed to resize Chinese text. ${err}`);
+      if(test_mode) console.log(`Failed build and send content script object. ${err}`);
     }
 
-    // reset form values
-    newFontSizeInput.val('');
+    // reset form input values
+    //newFontSizeInput.val('');
     errorMessage.html('');
   })
 })
