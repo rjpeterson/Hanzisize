489de1a3fa0b95d303e011bd42e88b247606dd37
rjpeterson <r.james.peterson@gmail.com>
2020-08-01 21:21:23 +0800

    add labels to inputs and fix flexboxing

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -17,11 +17,11 @@ class MoreInfo extends React.Component {
       <div className="more-info-container">
         <div className="show-more" onClick={this.handleClick}>{this.props.seeMore ? '<Go Back>': '<Show More Info>'}</div>
         <div className={this.props.seeMore ? 'info': 'inactive'}>
-          <p>Enjoying using Hanzisize?</p>
+          <p>Hi there. I'm Ryan, the developer behind Hanzisize. I've developed this extension and made it available for anyone to use for free. If you appreciate my work, please consider donating so I can continue to improve this tool for all of us.</p>
           <p>Donate here: paypal.me/nolapiano</p>
           <p>or with bitcoin:</p>
           <img alt="a bitcoin qr code" src={btccode} width="100" height="100"></img>
-          <p>You can contribute to the project here:</p>
+          <p>You can contribute code or report bugs here:</p>
           <p>https://github.com/rjpeterson/Hanzisize</p>
         </div>
       </div>
