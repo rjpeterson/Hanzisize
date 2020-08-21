f350b053e969c2b5639456e1c56af2285b70b77f
rjpeterson <r.james.peterson@gmail.com>
2020-08-21 23:12:59 +0800

    add  button

--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -18,7 +18,9 @@ class MoreInfo extends React.Component {
         <div className="show-more" onClick={this.handleClick}>{this.props.seeMore ? '<Go Back>': '<Show More Info>'}</div>
         <div className={this.props.seeMore ? 'info': 'inactive'}>
           <p>Hi there. I'm Ryan, the developer behind Hanzisize. I've developed this extension and made it available for anyone to use for free. If you appreciate my work, please consider donating so I can continue to improve this tool for all of us.</p>
-          <p>Donate here: paypal.me/nolapiano</p>
+
+          <div className="coffee-button"><link href="https://fonts.googleapis.com/css?family=Cookie" rel="stylesheet"></link><a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/djmouthguard"><img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a matcha latte"></img><span style="margin-left:5px;font-size:24px !important;">Buy me a matcha latte</span></a></div>
+
           <p>or with bitcoin:</p>
           <img alt="a bitcoin qr code" src={btccode} width="100" height="100"></img>
           <p>You can contribute code or report bugs here:</p>
